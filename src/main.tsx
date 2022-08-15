f01c2cd1a9367824354d236d6ccb70e94eec3955
Aelita <xsjctony@126.com>
2022-08-15 14:59:56 +1000

    Change the app root to document.body

--- src/main.tsx
+++ src/main.tsx
@@ -11,13 +11,8 @@ import eruda from 'eruda' // TODO: remove on production
 
 eruda.init() // TODO: remove on production
 
-const root = document.querySelector('#app')
 
-if (!root) {
-  throw new Error('Root element not found')
-}
-
-createRoot(root).render(
+createRoot(document.body).render(
   <StrictMode>
     <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
       <App />
