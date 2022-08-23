285fc9fa1a50095fc478a605930740ce65bfdec7
Aelita <xsjctony@126.com>
2022-08-23 14:32:31 +1000

    Get random solution

--- src/store/useGameState.ts
+++ src/store/useGameState.ts
@@ -3,6 +3,7 @@ import create from 'zustand'
 import { persist } from 'zustand/middleware'
 import { immer } from 'zustand/middleware/immer'
 import { BoardLetterState, GameStatus, GUESS_CHANCE, WORD_LENGTH } from '@/constants'
+import { getSolution } from '@/utils'
 import type { Alphabet, Tuple } from '@/utils/types'
 
 
@@ -41,22 +42,22 @@ type GameState = GameStateAction & GameStateData
 /**
  * Initial state
  */
-const initialState: GameStateData = {
+const initialState = (): GameStateData => ({
   gameStatus: GameStatus.inProgress,
-  solution: 'humid', // TODO: change solution to generator
+  solution: getSolution(),
   boardState: new Array<string>(GUESS_CHANCE).fill('') as BoardState,
   evaluations: new Array(GUESS_CHANCE).fill([]) as Evaluations,
   currentRowIndex: 0,
   currentWord: [],
   evaluating: false
-}
+})
 
 
 /**
  * Store
  */
 const useGameState = create<GameState>()(persist(immer(set => ({
-  ...initialState,
+  ...initialState(),
   setEvaluationResult: (word: Alphabet[], result: EvaluationResult) => {
     set((state) => {
       state.boardState[state.currentRowIndex] = word.join('')
@@ -69,8 +70,8 @@ const useGameState = create<GameState>()(persist(immer(set => ({
   setGameStatus: (status: GameStatus) => void set({ gameStatus: status }),
   restartGame: () => {
     toast.dismiss()
-    set({ ...initialState, solution: 'humid' })
-  }, // TODO: change solution to generator
+    set({ ...initialState(), solution: getSolution() })
+  },
   insertLetter: (letter: Alphabet) => void set((state) => {
     if (state.currentWord.length !== 5) {
       state.currentWord.push(letter)
@@ -87,15 +88,14 @@ const useGameState = create<GameState>()(persist(immer(set => ({
   name: 'game-state',
   getStorage: () => localStorage,
   partialize: (state) => {
-    const { gameStatus, solution, boardState, evaluations, currentRowIndex, currentWord } = state
+    const { gameStatus, solution, boardState, evaluations, currentRowIndex } = state
 
     return {
       gameStatus,
       solution,
       boardState,
       evaluations,
-      currentRowIndex,
-      currentWord // For resetting the game after page reloads
+      currentRowIndex
     }
   }
 }))
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,10 @@
 import dictionary from '@/data/dictionary.json'
+import targetWords from '@/data/targetWords.json'
 import type { Alphabet } from '@/utils/types'
 
 
 export const validateWord = (word: Alphabet[]): boolean => dictionary.includes(word.join(''))
 
 export const compareFlatArray = (a: unknown[], b: unknown[]): boolean => a.length === b.length && a.every((v, i) => v === b[i])
+
+export const getSolution = (): string => targetWords[Math.floor(Math.random() * targetWords.length)]
