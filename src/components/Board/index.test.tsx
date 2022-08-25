e25c018079686d06d962fe8e502fa807a11ba1de
Aelita <xsjctony@126.com>
2022-08-25 19:08:36 +1000

    Test: Board

--- src/components/Board/index.test.tsx
+++ src/components/Board/index.test.tsx
@@ -1,1 +1,15 @@
-export {}
+import { describe, expect, it } from 'vitest'
+import { render, screen } from '@/test/utils'
+import Board from './'
+
+
+describe('Board', () => {
+  it('Render boardRows with flex layout with direction of column', () => {
+    render(<Board />)
+    const board = screen.getByLabelText(/game board/i)
+    const style = getComputedStyle(board)
+
+    expect(style.display).toBe('flex')
+    expect(style.flexDirection).toBe('column')
+  })
+})
--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -3,14 +3,11 @@ import { GUESS_CHANCE } from '@/constants'
 import styles from './index.module.scss'
 
 
-const Board = (): JSX.Element => {
-
-  return (
-    <div className={styles.board}>
-      {/* eslint-disable-next-line react/no-array-index-key */}
-      {new Array(GUESS_CHANCE).fill(void 0).map((_, i) => <BoardRow key={i} rowIndex={i} />)}
-    </div>
-  )
-}
+const Board = (): JSX.Element => (
+  <div aria-label="game board" className={styles.board}>
+    {/* eslint-disable-next-line react/no-array-index-key */}
+    {new Array(GUESS_CHANCE).fill(void 0).map((_, i) => <BoardRow key={i} rowIndex={i} />)}
+  </div>
+)
 
 export default Board
