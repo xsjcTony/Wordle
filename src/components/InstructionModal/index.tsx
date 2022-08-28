30834ff482bf82a9d4f4cae6fe623f4851b980ae
Aelita <xsjctony@126.com>
2022-08-29 05:11:06 +1000

    Improve instruction (features)

--- src/components/InstructionModal/index.tsx
+++ src/components/InstructionModal/index.tsx
@@ -98,8 +98,10 @@ const InstructionModal = ({ isOpen, closeModalHandler }: InstructionModalProps):
           <div>
             <p><strong>Other features</strong></p>
             <p>Dark Mode: Press <Sun /> or <Moon /> to switch dark mode ON / OFF.</p>
-            <p>Hard Mode: Press <strong className={styles.hardMode}>E</strong> or <strong className={styles.hardMode}>H</strong> to switch hard mode ON / OFF.</p>
-            <p>New Round: Press <Reset /> to start a new round. <i>You can only start a new round if you won or lost the current one.</i></p>
+            <p className={styles.featureWithDescription}>Hard Mode: Press <strong className={styles.hardMode}>E</strong> or <strong className={styles.hardMode}>H</strong> to switch hard mode ON / OFF.</p>
+            <p>Any revealed hints <strong>MUST</strong> be used in subsequent guesses.</p>
+            <p className={styles.featureWithDescription}>New Round: Press <Reset /> to start a new round.</p>
+            <p>You can <strong>ONLY</strong> start a new round if you won or lost the current one.</p>
           </div>
         </div>
       </section>
